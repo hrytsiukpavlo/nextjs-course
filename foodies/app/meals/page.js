26b6d70649443d92a6055ca99eaf6a0c62ecf9db
Pavlo Hrytsiuk <overdrive800392@gmail.com>
2025-09-14 12:35:51 +0200

    Section 3 WIP

--- foodies/app/meals/page.js
+++ foodies/app/meals/page.js
@@ -1,3 +1,28 @@
-export default function MealsPage() {
-  return <h1>This is meals page</h1>;
+import classes from "./page.module.css";
+import MealsGrid from "../components/meals/meals-grid";
+import Link from "next/link";
+import { getMeals } from "../../lib/meals";
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 }
