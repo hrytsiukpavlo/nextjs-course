0147b1eb4caf4309ffd09cd822168f035f98722a
Pavlo Hrytsiuk <overdrive800392@gmail.com>
2025-09-19 16:01:57 +0200

    Section 4 WIP

--- routing-rendering/app/news/page.js
+++ routing-rendering/app/news/page.js
@@ -1,20 +1,11 @@
-import Link from "next/link";
+import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/news-list";
 
 export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul>
-        <li>
-          <Link href="news/first">Dummy 1</Link>
-        </li>
-        <li>
-          <Link href="news/second">Dummy 2</Link>
-        </li>
-        <li>
-          <Link href="news/third">Dummy 3</Link>
-        </li>
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 }
