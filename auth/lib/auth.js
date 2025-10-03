9fd7f7950145c7a09bff71632e9e2c16496025db
Pavlo Hrytsiuk <overdrive800392@gmail.com>
2025-10-03 15:13:27 +0200

    Section 9 finished

--- auth/lib/auth.js
+++ auth/lib/auth.js
@@ -26,3 +26,58 @@ export async function createAuthSession(userId) {
     sessionCookie.attributes
   );
 }
+
+export async function verifyAuth() {
+  const sessionCookie = cookies().get(lucia.sessionCookieName);
+
+  if (!sessionCookie) {
+    return { user: null, session: null };
+  }
+
+  const sessionId = sessionCookie.value;
+
+  if (!sessionId) {
+    return { user: null, session: null };
+  }
+
+  const result = await lucia.validateSession(sessionId);
+
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+  } catch (err) {}
+
+  return result;
+}
+
+export async function destroySession() {
+  const { session } = await verifyAuth();
+
+  if (!session) {
+    return { error: "Unauthorized!" };
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+}
